10ec8f7a8e7a24be396fa6574eb11abf6762ef34
root <yaroslav.sib@yandex.ru>
2015-07-06 09:42:14 +0600

    add readme

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ $(function(){
 $(document).ready( function() {
 	
   var numCards = _.range(30),
-	cards   = [],
-	images  = ["barselona.jpg", "bran.jpg", "chichen_itza.jpg", "christ.jpg", 
-				"colizey.jpg", "elf_tower.jpg", "great_wall.jpg", "himedzi.jpg",
-				"liberty.jpg", "mahal.jpg", "moai.jpg", "palace.jpg", "piramid.jpg",
-				"sobor.jpg", "stonehandge.jpg"],
-	matched = [],
-	showing = [], // elements that show their real image now 
-	attempts = 0; // number of trials
+    cards   = [],
+    images  = ["barselona.jpg", "bran.jpg", "chichen_itza.jpg", "christ.jpg", 
+               "colizey.jpg", "elf_tower.jpg", "great_wall.jpg", "himedzi.jpg",
+               "liberty.jpg", "mahal.jpg", "moai.jpg", "palace.jpg", "piramid.jpg",
+               "sobor.jpg", "stonehandge.jpg"],
+    matched = [],
+    showing = [], // elements that show their real image now 
+    attempts = 0; // number of trials
 
   $('body').disableSelection();
 	
@@ -35,47 +35,47 @@ $(document).ready( function() {
 	  cards[i].img = img; // mix images and set mixed values to 'img' properties
 	});
 		
-	makeCards();
+    makeCards();
 		
   }
 	
   function Card(id) {
-	this.img;
-	this.id = id;
-	this.class = 'card';
+    this.img;
+    this.id = id;
+    this.class = 'card';
   }
 
   /*
   create 30 <img> elements in card_container <div>
   */	
   function makeCards() {
-	_.each(cards, function(card) { 
+    _.each(cards, function(card) { 
   	  $('<img>', {
-		class: card.class,
-		src: 'images/back.jpg',
-		id: card.id
+	    class: card.class,
+	    src: 'images/back.jpg',
+	    id: card.id
 	  }).on('click', function() {
-			showing.push(cards[card.id]); // turn out on click and add to showing array;
-			flipCard(this);
-		}).appendTo("#card_container");
-	});
+		    showing.push(cards[card.id]); // turn out on click and add to showing array;
+		    flipCard(this);
+	    }).appendTo("#card_container");
+    });
   }
   
   function flipCard(card) {
-	$('#' + card.id).attr('src', 'images/' + cards[card.id].img); // change src of picked up image to real one
-	$(card).off('click'); // we no more need user to click on this image until the second image do not selected
+    $('#' + card.id).attr('src', 'images/' + cards[card.id].img); // change src of picked up image to real one
+    $(card).off('click'); // we no more need user to click on this image until the second image do not selected
 		
-	if (showing.length == 2) {
+    if (showing.length == 2) {
 	  checkCards();
 	}
   }
 	
   function checkCards() {
-	attempts++;
+    attempts++;
 		
-	if (showing[0].img == showing[1].img) {
+    if (showing[0].img == showing[1].img) {
 	  _.each(showing, function(card) { 
-		matched.push(card);
+	    matched.push(card);
 	  });
 	  // if selected images are the same, show the information about picture from the description file
 	  $('#wonder_photo').attr("src", 'images/' + showing[0].img);
@@ -84,24 +84,24 @@ $(document).ready( function() {
 	  $('.modal').fadeIn(400);
 	  showing = [];
 			
-	  if (matched.length == images.length) { // if image pair is last one - show information and when show the results block
-		$('.close').off();
-		$('.close').on("click", function () {
-		  $('#description').hide();
+      if (matched.length == images.length) { // if image pair is last one - show information and when show the results block
+        $('.close').off();
+        $('.close').on("click", function () {
+    	  $('#description').hide();
 		  showResults();
 		  });
 	  }
 	} else {
-		setTimeout(function() { // if images are not the same - turn them and set initial onclick function
+	    setTimeout(function() { // if images are not the same - turn them and set initial onclick function
 		  _.each(showing, function(card) {
-			$('#' + card.id).on('click', function() {
+		    $('#' + card.id).on('click', function() {
 			  showing.push(cards[card.id]);
 			  flipCard(this);
 			  }).attr('src', 'images/back.jpg')
 		  });
 		  showing = [];
-		}, 700);
-	}
+	    }, 700);
+    }
 		
     updateStats();
 		
@@ -120,18 +120,18 @@ $(document).ready( function() {
 	  $('#end').show();
 	  $('.close').on("click", function () {
 	    $('#end').fadeOut(400);
-	  });
-	}
+      });
+    }
   }
 	
   function updateStats() {
   	// change values on statistic block
-	$('#attempts').text('Попыток: ' + attempts);
-	$('#matches').text('Отгадано: ' + matched.length/2);
+    $('#attempts').text('Попыток: ' + attempts);
+    $('#matches').text('Отгадано: ' + matched.length/2);
   }
 
   $('.close').on("click", function () {
-	$('.modal').fadeOut(400); //close the div, when user push X
+    $('.modal').fadeOut(400); //close the div, when user push X
   });
 	
 });
